f167ced9bc7b1f1b8b4bf96d3137503188e83f2b
Jim Eckerlein <jim.eckerlein@gmail.com>
2020-09-28 18:55:27 +0200

    nonvariadic FunctionReturnType

--- flow/flow/TypeTraits.h
+++ flow/flow/TypeTraits.h
@@ -8,7 +8,7 @@
 
 namespace flow::details
 {
-    template<class F, class... T>
+    template<class F, class T>
     using FunctionReturnType = decltype(std::declval<F>()(
         // This statically calls the functional type with the iterator's
         // value type, to get the functions return type since
@@ -16,6 +16,6 @@ namespace flow::details
         // This means that the iterator's value type must be convertible
         // to the functional's argument type, like:
         // `T -> T` or `T -> const T&`, but not `T -> T&`.
-        std::declval<T>()...
+        std::declval<T>()
     ));
 }
